c871352cb34863063bf0080ff783e78feea0b606
lunnaviana05 <lunnaviana05@gmail.com>
2023-02-28 14:53:27 +0000

    trabalho dois

---
+++ Programa.java
@@ -0,0 +1,16 @@
+public class Programa {
+    public static void main(String[] args){
+        conta minhaconta;
+        minhaconta = new conta();
+        minhaconta.numero=1;
+        minhaconta.titular="Lunna Viana";
+        minhaconta.saldo=100;
+
+        conta minhaconta2;
+        minhaconta = new conta();
+        minhaconta.numero=2;
+        minhaconta.titular="Selene Bitto";
+        minhaconta.saldo=2000;
+
+    }
+}
---
+++ conta.java
@@ -0,0 +1,6 @@
+public class conta {
+        int numero;
+        String titular;
+        double saldo;
+    }
+}
